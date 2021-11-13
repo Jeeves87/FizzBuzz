8b0bdfe8aafb62fc8fc9ab6d6919005ae90c9124
Jeeves87 <reevesj812@gmail.com>
2021-11-12 19:11:28 -0600

    Add project files.

---
+++ GetArray.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class GetArray
+    {
+        int UserStart;
+        int UserSize;
+        private int[] _userArray;
+        public int[] userArray
+        {
+            get
+            {
+                return _userArray;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _userArray = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+        
+
+        public int[] MakeArray()
+        {
+           int[] UserArray = Enumerable.Range(UserStart, UserSize).ToArray();
+            return UserArray;
+        }
+
+        public GetArray(string userStart, string userSize)
+        {
+            bool success = int.TryParse(userStart, out UserStart);
+            if (success)
+            { Console.WriteLine("Your entry was a number!"); }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }
+            bool success2 = int.TryParse(userSize, out UserSize);
+            if (success)
+            { Console.WriteLine("Your entry was a number!"); }
+            else
+            {
+                Console.WriteLine("Conversion failed");
+            }     
+        }
+
+    
+        
+
+    
+        
+        
+
+
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz!");
+            Console.WriteLine("FizzBuzz seeks to find out if any number from a series of numbers is divisible by 3, 5 or both.");
+            Console.WriteLine("Please enter a number you would like to start from.");
+            string Input1 = Console.ReadLine();
+            Console.WriteLine("Thank you! Now enter a size for the array (ex: 100)");
+            string Input2 = Console.ReadLine();
+            GetArray GetArray = new(Input1, Input2);
+            int[] UserArray = GetArray.userArray;
+            UserArray = GetArray.MakeArray();
+            foreach (int i in UserArray)
+            {
+                if ((i % 15) == 0)
+                {
+                    Console.WriteLine($"{i}- Fizz Buzz!");
+                    continue;
+                }
+
+
+                if ((i % 5) == 0)
+                {
+                    Console.WriteLine($"{i}- Buzz!");
+                    continue;
+                }
+                if ((i % 3) == 0)
+                {
+                    Console.WriteLine($"{i}- Fizz!");
+                    continue;
+                }
+
+                             
+            }
+                
+        }
+    }
+}
